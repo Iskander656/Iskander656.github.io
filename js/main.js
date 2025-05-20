b9d597defbc6a2acb428270cfd7c165eb0514030
Iskander <iskander@Iskanders-MacBook-Air.local>
2025-05-20 19:16:47 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let InstagramBtn = document.getElementById("InstagramButton");
+InstagramBtn.addEventListener("click", function (){
+    window.open("https://www.instagram.com/yourusername", "_blank");
+    
+});
+let TelegramBtn = document.getElementById("TelegramButton");
+TelegramBtn.addEventListener('click', function (){
+    window.open("https://t.me/yourusername", "_blank");
+})
