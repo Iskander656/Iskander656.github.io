a36f8f71a5060b1934751ec82cf3793bd262d3d1
Iskander <iskander@Iskanders-MacBook-Air.local>
2025-05-21 23:16:21 +0500

    1.0.1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let InstagramBtn = document.getElementById("InstagramButton");
 InstagramBtn.addEventListener("click", function (){
-    window.open("https://www.instagram.com/yourusername", "_blank");
+    window.open("https://www.instagram.com/is__a_l_e_x_a_n_d_e_r?igsh=amx5Mzd0cnE2NWRq", "_blank");
     
 });
 let TelegramBtn = document.getElementById("TelegramButton");
 TelegramBtn.addEventListener('click', function (){
     window.open("https://t.me/yourusername", "_blank");
-})
+});
+let GitHubBtn = document.getElementById('GitHubButton');
+GitHubBtn.addEventListener('click', function(){
+    window.open("https://github.com/Iskander656", "_blank");
+});
