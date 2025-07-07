45bd1315116d1deeeccb1614acd2299edea71853
Iskander <sopyyewiskander@gmail.com>
2025-07-08 00:29:50 +0500

    1.1.0

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 let InstagramBtn = document.getElementById("InstagramButton");
-InstagramBtn.addEventListener("click", function (){
-    window.open("https://www.instagram.com/is__a_l_e_x_a_n_d_e_r?igsh=amx5Mzd0cnE2NWRq", "_blank");
-    
+InstagramBtn.addEventListener("click", function () {
+  window.open(
+    "https://www.instagram.com/is__a_l_e_x_a_n_d_e_r?igsh=amx5Mzd0cnE2NWRq",
+    "_blank"
+  );
 });
 let TelegramBtn = document.getElementById("TelegramButton");
-TelegramBtn.addEventListener('click', function (){
-    window.open("https://t.me/yourusername", "_blank");
+TelegramBtn.addEventListener("click", function () {
+  window.open("https://t.me/yourusername", "_blank");
 });
-let GitHubBtn = document.getElementById('GitHubButton');
-GitHubBtn.addEventListener('click', function(){
-    window.open("https://github.com/Iskander656", "_blank");
+let GitHubBtn = document.getElementById("GitHubButton");
+GitHubBtn.addEventListener("click", function () {
+  window.open("https://github.com/Iskander656", "_blank");
 });
